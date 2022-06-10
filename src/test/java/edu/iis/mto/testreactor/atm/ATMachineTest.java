8d2414ec63ad7a2e900e908db03c6448c61c6fb9
piotr-s-nowak <217728@edu.p.lodz.pl>
2022-06-10 18:21:48 +0200

    created test for successful withdrawal

--- src/test/java/edu/iis/mto/testreactor/atm/ATMachineTest.java
+++ src/test/java/edu/iis/mto/testreactor/atm/ATMachineTest.java
@@ -1,19 +1,47 @@
 package edu.iis.mto.testreactor.atm;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import edu.iis.mto.testreactor.atm.bank.Bank;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.Currency;
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
 class ATMachineTest {
+    @Mock
+    Bank bank;
 
+    Card card;
+    PinCode pin;
+    ATMachine atm;
+    List<BanknotesPack> banknotes;
     @BeforeEach
-    void setUp() throws Exception {}
 
-    @Test
-    void itCompiles() {
-        assertThat(true, Matchers.equalTo(true));
+    void setUp() throws Exception {
+        atm = new ATMachine(bank, Currency.getInstance("PLN"));
+        banknotes = new ArrayList<>();
+        pin = PinCode.createPIN(1,2,3,4);
+        card = Card.create("123456789");
     }
 
-}
+    @Test
+    void successfulWithdrawal() throws ATMOperationException {
+        BanknotesPack tens = BanknotesPack.create(100, Banknote.PL_20);
+        banknotes.add(tens);
+        MoneyDeposit deposit = MoneyDeposit.of(atm.getCurrentDeposit().getCurrency(), banknotes);
+        atm.setDeposit(deposit);
+
+        List<BanknotesPack> correctWithdraw = new ArrayList<>();
+        correctWithdraw.add(BanknotesPack.create(3, Banknote.PL_20));
+
+        assertEquals(atm.withdraw(pin, card, new Money(60)), Withdrawal.create(correctWithdraw));
+    }
